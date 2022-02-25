7193bb1f3e0f7e86fe129032e09484e3ba69c84e
Arpit Arya <87825317+ArpAry@users.noreply.github.com>
2022-02-26 01:26:07 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+     {
+         int a,b,c,li=0,d,l=-1,m;
+         printf("Enter the value of a ");
+         scanf("%d",&a);
+         d=a;
+         m=a;
+         while(m!=0)
+         {
+             m=m/2;
+             li++;
+         }
+         while(d!=0)
+         {
+             c=d%2;
+             d=d/2;
+             l++;
+            scanf("%d"&ar[l])
+             }
+             printf("%d",c);
+         }
+         return 0;
+
+     }
